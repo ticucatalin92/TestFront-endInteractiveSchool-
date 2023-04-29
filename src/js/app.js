032322e9c03da69558433d5c7d0ee22c5bddf4e9
ticucatalin92 <ticucatalin92@gmail.com>
2023-04-29 11:03:34 +0300

    Scroller Carousel structure
    create the structure of carousel

--- src/js/app.js
+++ src/js/app.js
@@ -1,8 +1,12 @@
 import {getDataHeroCarousel}from "./components/heroCarousel.js";
+import {getDataForCarouselScroller} from "./components/scrollerCarousel.js";
 document.addEventListener("DOMContentLoaded", () => {
     // hide warning message if javascript is enabled
     document.getElementById("no-js").classList.add("m-hide");
 
     //Create Hero Carousel
     getDataHeroCarousel();
+
+    //Create Scroller Carousel
+    getDataForCarouselScroller();
 });
